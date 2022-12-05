035af8d3ce4968a3af8c869e27f04ad86f95aa5b
Adiya21j <adiya21j@gmail.com>
2022-12-05 17:06:27 +0800

    Burger ingredient is done

--- src/components/Toolbar/index.js
+++ src/components/Toolbar/index.js
@@ -1,5 +1,12 @@
 import React from "react";
+import css from "./style.module.css"
 
-const Toolbar = () => <div>Toolbar</div>;
+const Toolbar = () => (
+    <header className={css.Toolbar}>
+        <div>...</div>
+        <div>Лого</div>
+        <div>Цэс</div>
+    </header>
+);
 
 export default Toolbar;
